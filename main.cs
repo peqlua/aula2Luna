f90394b466b37e9c0ad46a079d70ad0cc4c043ad
pablo erick erick <19280925-pablo-erickeric@users.noreply.replit.com>
2024-08-19 13:30:41 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program {
+
+ 
+  
+  public static void Main (string[] args) {
+ int agePaiblo = 41;   
+ int ageMama = 42;
+ int ageLuna = 8;
+ int ageLis = 12;
+
+    
+    Console.WriteLine (ageLis - ageLuna);
+  }
+}
